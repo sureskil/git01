71ff4167c8c11a26028101edd6362dc4ffbcacce
myskil <2844601032@qq.com>
2023-07-06 17:20:35 +0800

    dev分支，添加Test01.java

--- src/main/java/org/example/Hello2.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-public class Hello2 {
-    public static void main(String[] args) {
-        System.out.println("大家好");
-    }
-}
--- src/main/java/org/example/Test01.java
+++ src/main/java/org/example/Test01.java
@@ -1,7 +1,7 @@
 package org.example;
 
-public class Stop {
+public class Test01 {
     public static void main(String[] args) {
-        System.out.println("stop");
+        System.out.println("Test01...");
     }
 }
