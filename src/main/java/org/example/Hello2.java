c600980c674d0e6da7c148be99b9ba9fdde77b5c
myskil <2844601032@qq.com>
2023-07-06 16:40:09 +0800

    第7次修改，添加Hello2

---
+++ src/main/java/org/example/Hello2.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("大家好");
+    }
+}
