8b507f0e55b7b67702fa00381451a36cf62ef63b
myskil <2844601032@qq.com>
2023-07-06 17:44:46 +0800

    dev修改

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("你好啊");
         System.out.println("你好啊2");
-        System.out.println("dev ->  xx");
+        System.out.println("dev ->  xy");
     }
 }
