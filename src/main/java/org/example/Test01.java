2e46b0beffeb3f9245a8edc6a762256ce72b3959
myskil <2844601032@qq.com>
2023-07-07 11:45:31 +0800

    right1

--- src/main/java/org/example/Test01.java
+++ src/main/java/org/example/Test01.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println("Test01...right2");
+        System.out.println("Test01...right3");
     }
 }
