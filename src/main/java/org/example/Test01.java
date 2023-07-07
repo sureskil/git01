671fdd6a30013773f0ee9b27a1334937747c5c2b
myskil <2844601032@qq.com>
2023-07-07 11:27:47 +0800

    7.7,dev第一次修改

--- src/main/java/org/example/Test01.java
+++ src/main/java/org/example/Test01.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println("Test01...");
+        System.out.println("Test01...right");
     }
 }
